1932e008a6eb299e2c53f6a8f64161a406861bc9
Wyatt Henderson <henderson.wyatt95@gmail.com>
2020-09-23 18:45:04 -0400

    Updates

---
+++ precache-manifest.eb48c5f10092621f0bc2868319ccaf06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d44c6b315b6f2f836ffda790c378c76",
+    "url": "/todolist_react/index.html"
+  },
+  {
+    "revision": "dea09c277522eeca3078",
+    "url": "/todolist_react/static/css/main.f5d11db2.chunk.css"
+  },
+  {
+    "revision": "fcf0bfbdb91c046bd9df",
+    "url": "/todolist_react/static/js/2.b0b2a54b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist_react/static/js/2.b0b2a54b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dea09c277522eeca3078",
+    "url": "/todolist_react/static/js/main.85e05349.chunk.js"
+  },
+  {
+    "revision": "ebb7289f9ea588ec1da5",
+    "url": "/todolist_react/static/js/runtime-main.0f19723e.js"
+  },
+  {
+    "revision": "fd384f8b34388d5c8c7d337c9f228d06",
+    "url": "/todolist_react/static/media/lightbeams.fd384f8b.jpg"
+  },
+  {
+    "revision": "75815c532d7ad4b280921c83f0f7af56",
+    "url": "/todolist_react/static/media/lightbeams_rev.75815c53.jpg"
+  }
+]);
